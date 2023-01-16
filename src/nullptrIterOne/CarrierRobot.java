28c75d7230eb71c12caf1e9e8fc411155c32c758
Connor Bramhall <41408864+Connorwb@users.noreply.github.com>
2023-01-16 18:47:45 -0500

    Submission 1

--- src/nullptrIterOne/CarrierRobot.java
+++ src/nullptrIterOne/CarrierRobot.java
@@ -8,6 +8,7 @@ public class CarrierRobot extends MovingRobot {
 	MapLocation PatrolPoint;
 	
 	public CarrierRobot(RobotController rc) {
+		super(rc);
 		this.rc = rc;
 	}
 	
@@ -15,25 +16,60 @@ public class CarrierRobot extends MovingRobot {
 	void takeTurn() {
 		while (true) {
 			try {
-				for (int i = 0; (rc.getRoundNum() % 5 == 0) && i < 64; i++) {
+				if (HomeBase == null) {
+					HomeBase = rc.adjacentLocation(Direction.SOUTHEAST);		
+				}
+				for (int i = 0; (rc.getRoundNum() % 5 == 0) && rc.canWriteSharedArray(0, 0) && i < 64; i++) {
 					if ((rc.readSharedArray(i) & 0b1110000000000100) == 0b0110000000000000) {
 						int toPublish = rc.readSharedArray(i) | 0b0000000000000100;
 						rc.writeSharedArray(i, toPublish);
 						PatrolPoint = new MapLocation(((rc.readSharedArray(i) & 0b0001111100000000) >> 8), ((rc.readSharedArray(i) & 0b0000000011111000) >> 3));
+						setTargetLoc(PatrolPoint);
 						rc.setIndicatorString("Going to caravan point x: " + PatrolPoint.x + ", y: " + PatrolPoint.y);
 						i = 64;
 					}
 				}
+				MapLocation me = rc.getLocation();
+		        for (int dx = -1; dx <= 1; dx++) {
+		            for (int dy = -1; dy <= 1; dy++) {
+		                MapLocation wellLocation = new MapLocation(me.x + dx, me.y + dy);
+		                if (rc.canCollectResource(wellLocation, -1)) {
+		                    if (rng.nextBoolean()) {
+		                        rc.collectResource(wellLocation, -1);
+		                        rc.setIndicatorString("Collecting, now have, AD:" + 
+		                            rc.getResourceAmount(ResourceType.ADAMANTIUM) + 
+		                            " MN: " + rc.getResourceAmount(ResourceType.MANA) + 
+		                            " EX: " + rc.getResourceAmount(ResourceType.ELIXIR));
+		                    }
+		                }
+		            }
+		        }
+		        int weight = (rc.getResourceAmount(ResourceType.ADAMANTIUM) + rc.getResourceAmount(ResourceType.MANA) + rc.getResourceAmount(ResourceType.ELIXIR));
 				if (PatrolPoint == null) {
 					rc.setIndicatorString("Exploring");
-					explore(rc);
+					explore();
 					WellInfo[] searchResults = rc.senseNearbyWells();
 					if (searchResults.length > 0) {
 						PatrolPoint = searchResults[0].getMapLocation();
+						setTargetLoc(PatrolPoint);
 						rc.setIndicatorString("Mining " + searchResults[0].getResourceType().toString());
 					}
 				} else {
-					System.out.println("Patrol Point x: " + PatrolPoint.x + ", y: " + PatrolPoint.y);
+				    if (weight > 30) {
+			        	rc.setIndicatorString("Going Back to Base.");
+			        	setTargetLoc(HomeBase);
+				    } 
+				    if (weight == 0) {
+				    	setTargetLoc(PatrolPoint);
+				    	//TODO: Directional, instead of just at the location.
+					} else if (rc.canTransferResource(HomeBase, ResourceType.ADAMANTIUM, rc.getResourceAmount(ResourceType.ADAMANTIUM)))  {
+						rc.transferResource(HomeBase, ResourceType.ADAMANTIUM, rc.getResourceAmount(ResourceType.ADAMANTIUM));
+					} else if (rc.canTransferResource(HomeBase, ResourceType.MANA, rc.getResourceAmount(ResourceType.MANA)))  {
+						rc.transferResource(HomeBase, ResourceType.MANA, rc.getResourceAmount(ResourceType.MANA));
+					} else if (rc.canTransferResource(HomeBase, ResourceType.ELIXIR, rc.getResourceAmount(ResourceType.ELIXIR)))  {
+						rc.transferResource(HomeBase, ResourceType.ELIXIR, rc.getResourceAmount(ResourceType.ELIXIR));
+					}
+					moveTo();
 				}
 			} catch (GameActionException e) {
 	            System.out.println(rc.getType() + " Exception");
--- src/nullptrIterOne/MovingRobot.java
+++ src/nullptrIterOne/MovingRobot.java
@@ -21,15 +21,15 @@ public class MovingRobot extends Behavior{
 	private int mapLocY;
 	private MapLocation curLoc;
 	
-	public MovingRobot() {
-
+	public MovingRobot(RobotController rc) {
+		this.rc = rc;
 	}
 	
 	@Override 
 	void takeTurn() {
 	}
 	
-	void explore(RobotController rc) throws GameActionException {	
+	void explore() throws GameActionException {	
 	    Direction dir = directions[rng.nextInt(directions.length)];
         if (rc.canMove(dir)) {
             rc.move(dir);
@@ -53,34 +53,37 @@ public class MovingRobot extends Behavior{
 		mapLocX = x;
 		mapLocY = y;
 	}
+	
+	public void setTargetLoc(MapLocation target) {
+		mapLocX = target.x;
+		mapLocY = target.y;
+	}
 
 	public void setMapLocY(int mapLocY) {
 		this.mapLocY = mapLocY;
 	}
-
-	public MovingRobot(RobotController rc) {
-		this.rc = rc;
-		
+	
+	public void moveTo() throws GameActionException {
+		curLoc = rc.getLocation();
+		if(curLoc.x > mapLocX && rc.canMove(Direction.WEST)) {
+			rc.move(Direction.WEST);
+		}
+		else if(curLoc.x < mapLocX && rc.canMove(Direction.EAST)) {
+			rc.move(Direction.EAST);
+		}
+		if(curLoc.y > mapLocY && rc.canMove(Direction.SOUTH)) {
+			rc.move(Direction.SOUTH);
+		}
+		else if(curLoc.y < mapLocY && rc.canMove(Direction.NORTH)) {
+			rc.move(Direction.NORTH);
+		}
+		if (rc.getRoundNum() % 3 == 0) {
+			explore();
+		}
 	}
 	
-	public void takeTurn() {
-		curLoc = rc.getLocation();
-		try {
-			if(curLoc.x > mapLocX) {
-				rc.move(Direction.WEST);
-			}
-			if(curLoc.x < mapLocX) {
-				rc.move(Direction.EAST);
-			}
-			if(curLoc.y > mapLocY) {
-				rc.move(Direction.SOUTH);
-			}
-			if(curLoc.y < mapLocY) {
-				rc.move(Direction.NORTH);
-			}
-		}catch (Exception e) {
-			System.out.println("uhh oh spagiety ooos");
-		}
-		
+	public void moveTo(MapLocation target) throws GameActionException {
+		setTargetLoc(target);
+		moveTo();
 	}
 }
